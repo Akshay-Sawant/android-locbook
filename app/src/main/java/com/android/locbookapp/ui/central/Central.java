fc26088a076c858ef5d74de509e25211d38db2ed
Akshay Sawant <akshay@cloudwalker.tv>
2021-06-21 00:31:01 +0530

    Created Central.java with respect to its ui

---
+++ app/src/main/java/com/android/locbookapp/ui/central/Central.java
@@ -0,0 +1,26 @@
+package com.android.locbookapp.ui.central;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.locbookapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Central extends Fragment {
+    public Central() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_central, container, false);
+    }
+
+}
