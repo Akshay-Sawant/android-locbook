9aa0d05431ba4ab92325bbdc381a0f8a95b93012
Akshay Sawant <akshay@cloudwalker.tv>
2021-06-20 23:26:30 +0530

    AppInfoPage: - created the file with request to its ui - added circle image view dependency to the build.gradle file

---
+++ app/src/main/java/com/android/locbookapp/ui/appinfopage/AppInfoPage.java
@@ -0,0 +1,16 @@
+package com.android.locbookapp.ui.appinfopage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.locbookapp.R;
+
+public class AppInfoPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_info_page);
+    }
+}
