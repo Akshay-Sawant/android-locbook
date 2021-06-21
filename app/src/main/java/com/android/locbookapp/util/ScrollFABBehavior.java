2cb8aa40f915337856e6fdf81de5200a41f8075c
Akshay Sawant <akshay@cloudwalker.tv>
2021-06-22 01:22:21 +0530

    added ScrollFABBehavior to the util folder

---
+++ app/src/main/java/com/android/locbookapp/util/ScrollFABBehavior.java
@@ -0,0 +1,37 @@
+package com.android.locbookapp.util;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+/**
+ * Created by user on 25/12/2016.
+ */
+
+public class ScrollFABBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollFABBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide();
+        } else if (dyConsumed < 0 && child.getVisibility() == View.GONE) {
+            child.show();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
