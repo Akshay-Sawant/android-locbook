be3534612cc8d9e3e862bac954a17766acabfe9a
Akshay Sawant <akshay@cloudwalker.tv>
2021-06-21 22:09:16 +0530

    created files for Harbour module

---
+++ app/src/main/java/com/android/locbookapp/ui/harbour/Harbour.java
@@ -0,0 +1,28 @@
+package com.android.locbookapp.ui.harbour;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.locbookapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Harbour extends Fragment {
+    public Harbour() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_harbour, container, false);
+    }
+
+}
