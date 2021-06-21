6bd270adf04cba7cab139b46cfd6f10518746e27
Akshay Sawant <akshay@cloudwalker.tv>
2021-06-22 00:22:14 +0530

    created Registration module with respect to adding SmartTabLayout dependency to the build.gradle file

---
+++ app/src/main/java/com/android/locbookapp/ui/registration/RegistrationActivity.java
@@ -0,0 +1,45 @@
+package com.android.locbookapp.ui.registration;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import com.android.locbookapp.R;
+import com.android.locbookapp.ui.signin.Signin;
+import com.android.locbookapp.ui.signup.Signup;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    ViewPagerAdapter viewPagerAdapter;
+    CoordinatorLayout coordinatorLayout;
+    Context context;
+    String Username;
+    String Userid;
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private SmartTabLayout smartTabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_registration);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new Signin(), "Sign in");
+        viewPagerAdapter.addFragment(new Signup(), "Sign up");
+        viewPager.setAdapter(viewPagerAdapter);
+
+        smartTabLayout = (SmartTabLayout) findViewById(R.id.viewpager_tab);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
